e8bc634acb09ba5662b64362a1b444c189f53ecb
Siva <Zero@ruw-172.31.50.84.rutgers.edu>
2012-03-19 13:12:04 -0400

    Sum somewhat shows up

--- MasterSlave.c
+++ MasterSlave.c
@@ -3,45 +3,56 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <strings.h>
+#include <sys/file.h>
 
 void child(int row, int column, int array[row][column]);
-//void child(int *, int, int);
 void parent();
 
 int main() {
-	pid_t pid;	
+	pid_t pid;
+	int i=0,row =2 ,column =2,j,k,l=0;	
 	int array[2][2];
-	int i,row =2 ,column =2;
-	while (i<2) {
-		if (pid = fork())
-			child(2,2,array);
-		else
-			parent();
-		
-		i++;
+	for (j=0;j<2;j++) {
+		for (k=0;k<2;k++) {
+			array[j][k] = 1;
+		}
 	}
+	
+	//while (i<2) {
+		for (i;i<row;i++) {	
+			if (pid = fork())
+				child(i,2,array); 
+			else
+				parent();
+		//i++;
+	}
+	//child(0,2,array);
+	//child(1,2,array);
+	
 	return 0;
 }
 
-void child(int row, int column, int array[row][column]) {
-	int k,sum=0;
-	for (k=0;k<2;k++) {
+void child(int row, int column, int array[row][column]) { /*Child function splits the array into rows and*/ 
+	int k,l,sum=0,lockt;								  /*individually adds up the sum of the rows when it is called*/
+	FILE* file;
+	for (k=0;k<column;k++) {
 		sum += array[row][k];	
+		printf("%d\n",sum);
 	}
 	printf("I am the child.\n");
-	FILE* file;
-	file = fopen("somefile2.txt","a+");
+	file = fopen("somefile2.txt","w");
 	fprintf(file,"%d\n",sum);
 	fclose(file);	
 	exit(0);
+		return;
 }
 
-void parent() {
+void parent() { /*Adds up total using the results from child*/
 	FILE* newfile;
 	char *a; int i; char buffer[1024];
 	newfile = fopen("somefile2.txt","r");
-	while ( fgets(buffer,1024,newfile) > 0 )
-		printf("%s",buffer);
+	/*while ( fgets(buffer,1024,newfile) > 0 )
+		printf("%s",buffer);*/
 	
-	printf("I am the parent.");
+	printf("I am the parent.\n");
 }
